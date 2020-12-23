60a4d4d8bf7603887aa5ab54917acbf158be1082
joelmss93 <joelmss93@gmail.com>
2020-12-23 17:25:31 -0300

    Validando forms

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -65,7 +65,7 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container isFocused={isFocused}>
+    <Container isFocused={isFocused} isErrored={!!error}>
       <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
       <TextInput
         ref={inputElementRef}
